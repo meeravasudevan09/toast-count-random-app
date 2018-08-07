c7e5d38ab17ec76fda932cb63b535ccec5fe06f4
Meera Vasudevan <meera.vasudevan@rea-group.com>
2018-08-07 14:58:22 +1000

    Add functionality for Toast button
    -  Implement the click behaviour in MainActivity which takes in a single
    view
    - Set the method name as value of onClick property in View/Layout

--- app/src/main/java/com/example/meeravasudevan/toast_count_random_app/MainActivity.kt
+++ app/src/main/java/com/example/meeravasudevan/toast_count_random_app/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.meeravasudevan.toast_count_random_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onToastClick(view: View) {
+        val someText = "Yowza"
+        val someDuration = Toast.LENGTH_SHORT
+
+        Toast.makeText(this, someText, someDuration).show()
     }
 }
