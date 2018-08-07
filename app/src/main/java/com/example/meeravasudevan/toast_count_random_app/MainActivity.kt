74a8980940af0300833365edfbe1729213d3c749
Meera Vasudevan <meera.vasudevan@rea-group.com>
2018-08-07 15:29:01 +1000

    Add functionality for the Count button
    - Implement a method that taked in the view
      - gets the TextView reference by using findViewById
      - gets the value out of the reference
      - converts the value to int and then increments it
      - rewrites the TextView element with the new value onClick
    - Set the onClick property for the button to call this function

--- app/src/main/java/com/example/meeravasudevan/toast_count_random_app/MainActivity.kt
+++ app/src/main/java/com/example/meeravasudevan/toast_count_random_app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.meeravasudevan.toast_count_random_app
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +18,19 @@ class MainActivity : AppCompatActivity() {
         val someDuration = Toast.LENGTH_SHORT
 
         Toast.makeText(this, someText, someDuration).show()
+    }
+
+    fun onCountClick(view: View) {
+//        get text view reference
+        val textViewById = findViewById(R.id.number_text_view) as TextView
+
+//        get the value of that text view and convert to Int
+        var convertTextViewValueToInt = textViewById.text.toString().toInt()
+
+//        increment the int value
+        convertTextViewValueToInt++
+
+//        display the new value in the text view
+        textViewById.text = convertTextViewValueToInt.toString()
     }
 }
